97729c236d7da53748b1938ba4747a01c2ff1f70
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2017-05-10 15:28:29 +0200

    Update zsh plugins, fix path and add git to install list

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$DOTS/bin:$PATH"
+export PATH="/usr/local/bin:./bin:/usr/local/sbin:$DOTS/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
