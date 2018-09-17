f56115a86ba1b6cd751386648f7ca3aa30b1b414
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2018-09-17 18:40:15 +0200

    Update dotfiles

--- node/path.zsh
+++ node/path.zsh
@@ -3,3 +3,5 @@ mkdir -p ~/.nvm
 if [[ -a $(brew --prefix nvm)/nvm.sh ]]; then
     source $(brew --prefix nvm)/nvm.sh
 fi
+
+export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
