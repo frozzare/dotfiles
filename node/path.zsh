6094658f06bd1cefca2af9aea6571ff3b9740879
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2020-05-04 15:58:39 +0200

    Fix nvmn paths

--- node/path.zsh
+++ node/path.zsh
@@ -1,5 +1,5 @@
 mkdir -p ~/.nvm
 export NVM_DIR="$HOME/.nvm"
-[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"
-[ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
